aaf6e4fe7d0c1f4b701a3e5241224274d8bcdab2
TrueXPixels <truexpixels@gmail.com>
2020-04-09 11:14:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const GuardianClient = require('./core/client.js');
 const client = new GuardianClient();
 
-client.login('NjkwNjQ4ODk5MzM3MTkxNDk2.Xo9k2Q.T7wJH5mk1BNfCOGRLfZ7el8Ln1M');
+client.login('TOKEN');
