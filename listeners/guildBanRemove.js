4c35c3e947344ec18204d2d66a62f64bf1d7a300
Loren Cerri <lorencerri@gmail.com>
2020-11-11 05:38:00 -0800

    Update guildBanRemove.js

--- listeners/guildBanRemove.js
+++ listeners/guildBanRemove.js
@@ -9,21 +9,6 @@ module.exports = class GuildBanRemoveListener extends Listener {
     }
 
     async exec(guild, user) {
-        if (!guild) return;
-
-        // Fetch entry relating to action
-        let entry = await guild.find_entry(
-            'MEMBER_BAN_REMOVE',
-            e =>
-                e.target.id === user.id &&
-                e.createdTimestamp > Date.now() - 1000 * 60
-        );
-        if (!entry) return;
-
-        // Fetch entries (w/ entry prepended)
-        let entries = guild.push_entry(entry, user.tag);
-
-        // Check limits
-        guild.check_limits(entries, entry.executor.id, 'unbans');
+        console.log(`${user.tag} has been unbanned in ${guild.name}`);
     }
 };
