04f8f39a8f20bcc968d58ec4b278097f310090b6
Loren Cerri <lorencerri@gmail.com>
2020-11-11 05:12:44 -0800

    Update info.js

--- commands/general/info.js
+++ commands/general/info.js
@@ -14,7 +14,7 @@ class InfoCommand extends Command {
             .setTitle('Information')
             .setDescription(`  •  Current Prefix: `${message.guild.prefix}`\n  •  Invite me to your server [here](https://discord.com/oauth2/authorize?client_id=${this.client.id}&scope=bot)`);
         
-        return message.channel.send();
+        return message.channel.send(embed);
     }
 }
 
