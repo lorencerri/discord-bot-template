fc2b53e9a4e7c71537167b9bc5a4fc8131ea6e33
Loren Cerri <lorencerri@gmail.com>
2021-12-21 21:25:09 -0500

    Separate index.js file

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
 require('dotenv').config();
 require('@sapphire/plugin-logger/register');
 
-const { GatewayIntentBits } = require('discord-api-types/v9');
-const { SapphireClient } = require('@sapphire/framework');
-const { Constants } = require('discord.js');
-const { env } = require('./config');
-const colors = require('colorette');
+const { logClientIn } = require('./client.js');
+logClientIn();
 
-const client = new SapphireClient({
-    enableLoaderTraceLoggings: true,
-    intents: GatewayIntentBits.Guilds,
-    partials: [Constants.PartialTypes.CHANNEL],
-    disableMentions: 'everyone'
-});
-
-(async () => {
-    try {
-        await client.login(env.DISCORD_BOT_TOKEN);
-        client.logger.info(
-            colors.bold(colors.green('Successfully logged in...'))
-        );
-    } catch (error) {
-        client.logger.fatal(error);
-        client.destroy();
-        process.exit(1);
-    }
-})();
+// Hey there!
+// If you need any assistance,
+// feel free to join our Discord!
+// https://discord.gg/plexidev
